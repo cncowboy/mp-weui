7f6dd4bbc6467f77ba29b54fdc7da895b0f24605
zhangxiaobing <zhangxiaobing@51yund.com>
2018-07-17 11:30:02 +0800

    add native components

--- platforms/mp/native.js
+++ platforms/mp/native.js
@@ -1,31 +1,10 @@
-import wx from '@axetroy/wxapp-promisify';
+// import wx from '@axetroy/wxapp-promisify';
 
 /* eslint-disable */
 
-let Native = {};
-Native.install = function (Vue, options) {
-  let opt = {
-    defaultType:'bottom',
-    duration:'2500'
-  };
-  for(let property in options){
-    opt[property] = options[property];
-  }
-  function extend(opts, option) {
-    let opt = opts || {}
-    for (let key in opt) {
-      if (typeof opt[key] === 'object') {
-        for (let k in opt[key]) {
-          option[key][k] = opt[key][k]
-        }
-      } else {
-        option[key] = opt[key]
-      }
-    }
-    return option
-  }
+const Native = {}
+Native.install = function (Vue) {
 
-  Vue.prototype.$native = wx;
-};
+}
 
-Vue.use(Native);
+export default Native
