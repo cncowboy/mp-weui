51e76320f97aa2ed488067526211fc790d95589a
zhangxiaobing <zhangxiaobing@51yund.com>
2018-07-25 11:31:09 +0800

    modify switch file

--- platforms/web/native.js
+++ platforms/web/native.js
@@ -1,11 +1,9 @@
 import Picker from './picker'
-import MySwitch from './switch'
 
 /* eslint-disable */
 const Native = {}
 Native.install = function (Vue) {
   Vue.component('picker', Picker)
-  Vue.component('switch', MySwitch)
 
   Vue.prototype.$getPickerEventValue = (e, range) => {
     return e
